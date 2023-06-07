1650b9b3976e487dbb1f04e3eed351bdc4b50659
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2023-06-07 08:42:38 +0200

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,20 +3,30 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+  },
   root: true,
   overrides: [
     {
-      extends: [
-        "airbnb-base",
-        "airbnb-typescript/base",
-      ],
-      files: ['./**/*.{ts,tsx, cjs}'],
+      extends: ["airbnb-base", "airbnb-typescript/base"],
+      files: ["./**/*.{ts,tsx, cjs}"],
+      // rules: {
+      //   'max-len': ['error', 100, 2, {
+      //     ignoreUrls: true,
+      //     ignoreComments: false,
+      //     ignoreRegExpLiterals: true,
+      //     ignoreStrings: true,
+      //     ignoreTemplateLiterals: true,
+      //   }]
+      // },
 
       parserOptions: {
-        project: './tsconfig.json'
+        project: "./tsconfig.json",
       },
     },
   ],
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,5 @@ import app from './app';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-
 server.listen(port);
 console.log(port);
