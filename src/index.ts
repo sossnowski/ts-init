bd7fbff1e9292f4f85876cbb63fb37e6f70ea10a
Krzysztof Brzoska <krzysztof.brzoska@hawk-e.com.pl>
2023-06-07 09:02:25 +0200

    clean test code

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import app from './app';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-// eslint-disable-next-line prettier/prettier, @typescript-eslint/indent
 server.listen(port);
 // eslint-disable-next-line no-console
 console.log(port);
